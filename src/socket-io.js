8681b1af617f1cdfd63bed17c72faa1b99b165c5
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-05 16:41:13 -0300

    ✨ improve socket codes

--- src/socket-io.js
+++ src/socket-io.js
@@ -1,28 +1,30 @@
 import { Server } from 'socket.io';
+import { PORT } from './config.js';
 
 export const socketIO = async (server) => {
   try {
     const io = new Server(server, {
       cors: {
-        origin: 'http://localhost:8000',
+        origin: `http://localhost:${PORT}`,
         credentials: true,
       },
     });
 
     io.on('connection', (socket) => {
-      console.log('>>> Connected to socket: ', socket.id);
+      console.log(`>>> User connected to socket: ${socket.id}`);
+
       socket.on('disconnect', (param) => {
         console.log('>>> Disconnected', param);
       });
 
       // event that captures coding
-      socket.on('canalIn', ({ experiencia, canal, user }, callback) => {
+      socket.on('user_join_room', ({ experiencia, canal, user }, callback) => {
         socket.join(canal);
-        if (user) socket.broadcast.to(canal).emit('nuevoSubcriptor', user);
+        if (user) socket.broadcast.to(canal).emit('user_joined_room', user);
       });
 
-      socket.on('codeoEvent', function (data) {
-        socket.broadcast.to(data.canal).emit('codeoEmit', data);
+      socket.on('user_coding', function (data) {
+        socket.broadcast.to(data.canal).emit('user_code', data);
       });
     });
   } catch (error) {
