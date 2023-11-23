6fd21d707bee104d7ffc001c7cb1c2bfaf0affc3
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-22 23:57:09 -0300

    🔥 remove unnecessary schema

--- src/schemas/session.schema.js
+++ src/schemas/session.schema.js
@@ -14,15 +14,3 @@ export const updateSessionSchema = z.object({
     required_error: 'El ID de la sesion es requerido',
   }),
 });
-
-export const updateSessionObservacionSchema = z.object({
-  id: z.string({
-    required_error: 'El ID de la sesion es requerido',
-  }),
-  estado_observacion: z.string({
-    required_error: 'El estado de la observación de la sesion es requerido',
-  }),
-  observacion: z.string({
-    required_error: 'La observación de la sesion es requerida',
-  }),
-});
